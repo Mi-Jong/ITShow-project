5a85f468727920af81d6eb6aeae1a0a6627a7b5e
leeda06 <slmos33a@gmail.com>
2024-03-13 14:04:55 +0900

    :art: [feat] : 기본 구조 설정

---
+++ src/js/ranking.js
@@ -0,0 +1,19 @@
+import '../css/style.css';
+import Header from './header';
+import Footer from './footer';
+import '../css/ranking.css';
+
+
+function App() {
+    return (
+        <div>
+
+            <Header />
+
+            <Footer />
+
+        </div>
+    );
+}
+
+export default App;
