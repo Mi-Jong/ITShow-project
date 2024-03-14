d2f9ea926b3615611f00b6844f5eaa8e26b07010
leeda06 <slmos33a@gmail.com>
2024-03-14 12:05:10 +0900

    :lipstick: [feat] : virtual 파일 기능
    - 표와 뉴스 버튼

--- src/js/study-virtual.js
+++ src/js/study-virtual.js
@@ -4,31 +4,43 @@ import Header from './header';
 import '../css/study-virtual.css';
 
 function App() {
-    const [isHidden, setIsHidden] = useState(true);
+    const [nextHidden, setNextHidden] = useState(true);
+    const [line_table, setLine_table] = useState(false);
+    const [line_view, setLine_view] = useState(true);
 
-    const toggleHidden = () => {
-        setIsHidden(!isHidden);
+    const toggleNextHidden = () => {
+        setNextHidden(!nextHidden);
+    };
+    const toggleTableHidden = () => {
+        setLine_table(false);
+        setLine_view(true);
+    };
+    const toggleViewHidden = () => {
+        setLine_table(true);
+        setLine_view(false);
     };
 
     return (
         <div>
-
             <Header />
 
             <main>
                 <section className="part" id="part1">
                     <div className="logo"></div>
                     <div className="step">
-                        <button onClick={toggleHidden}></button>
+                        <button onClick={toggleNextHidden}></button>
                     </div>
                     <div className="info"></div>
                 </section>
 
                 <section className="part" id="part2">
                     <div className="graph"></div>
-                    <div className="button-line"></div>
-                    <div className="view" id="table"></div>
-                    {/* <div className="view hidden" id="news"></div> */}
+                    <div className="button-line">
+                        <button onClick={toggleTableHidden}>표</button>
+                        <button onClick={toggleViewHidden}>뉴스</button>
+                    </div>
+                    <div className="view" id={line_table ? 'hidden' : 'table'}></div>
+                    <div className="view" id={line_view ? 'hidden' : 'news'}></div>
                 </section>
 
                 <section className="part" id="part3">
@@ -38,12 +50,12 @@ function App() {
                 </section>
             </main>
 
-            <div className="detail" id={isHidden ? 'hidden' : ''}>
+            <div className={`detail`} id={nextHidden ? 'hidden' : ''}>
                 <div className="paper">
                     <div className='title'>결과화면</div>
                     <div className='result'></div>
                     <div className='next'>
-                        <button id='next-btn' onClick={toggleHidden}>다음화면</button>
+                        <button id='next-btn' onClick={toggleNextHidden}>다음화면</button>
                     </div>
                 </div>
             </div>
