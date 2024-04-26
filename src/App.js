998402c18fbf0b03b31b5e7430313965bbf15f57
leeda06 <slmos33a@gmail.com>
2024-04-26 14:54:59 +0900

    :poop: [chore]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 
 // import StudyVirtual from './js/study-virtual.js'; // StudyVirtual 컴포넌트 import
-import StudyGame from './components/study-game.js'; 
+// import StudyGame from './components/study-game.js'; 
+import StudyWord from './components/study-word.js'; 
+// import StudyWord from './components/index.js'; 
+
 
 function App() {
   return (
     <div>
-      <StudyGame />
+      <StudyWord />
     </div>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import '../css/style.css'; // 스타일 파일 import
+import Header from './header';
+import Footer from './footer';
+import '../css/index.css';
+
+function Section() {
+    return (
+        <div className='section'>
+            <div className='section-text'>
+                <p id='h1'>GEMMI</p>
+                <p id='h2'>
+                        주식 투자의 첫걸음을 떼는 당신을 위한 웹사이트
+                        “GEMMI”에서 주식 투자의 키초를 배우고,
+                        시뮬레이션을 통해 실전 감각을 키울 수 있습니다.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+
 
 function index() {
     return (
-        <header></header>
+        <div>
+            <Header/>
+            <Section/>
+            <Footer/>
+        </div>
     );
 }
 
