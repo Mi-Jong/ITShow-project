ed756eba1a0be1d2950049df2d3716fc7dbd31b4
leeda06 <slmos33a@gmail.com>
2024-05-14 08:08:55 +0900

    :sparkles: [feat] : 가상시뮬레이션 UI

--- src/components/study-virtual.js
+++ src/components/study-virtual.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
 import Header from './header';
 import '../css/style.css';
 import '../css/study-virtual.css';
@@ -6,56 +7,182 @@ import '../css/study-virtual.css';
 function Part1({ toggleNext }) {
     return (
         <section className="part" id="part1">
-            <div className="logo"></div>
-            <div className="step">
-                <button onClick={toggleNext}></button>
+            <div className='wer'>
+                <div className='balance'>
+                    <p className='bal-title'>내 잔고</p>
+                    <h2 className='bal-pay'>00000</h2>
+                </div>
+                <div className='purchase'>
+                    <button className='buy'>매수</button>
+                    <button className='sell'>매도</button>
+                </div>
             </div>
-            <div className="info"></div>
+            <ItemList/>
         </section>
     );
 }
-function Part2({isTableShown, setIsTableShown}) {
+
+function ItemList(){
+    const [items, setItems] = useState([
+      { name: 'Item 1', percentageIncrease: 10, price: 100 },
+      { name: 'Item 2', percentageIncrease: 15, price: 200 },
+      { name: 'Item 3', percentageIncrease: 20, price: 150 },
+    ]);
+    const [showItems, setShowItems] = useState(false);
+  
+    const calculateNewPrice = (price, percentageIncrease) => {
+      return price * (1 + percentageIncrease / 100);
+    };
+  
+    const toggleItems = () => {
+      setShowItems(!showItems);
+    };
+  
+    return (
+      <div className='list-stock'>
+        <h2 onClick={toggleItems} style={{ cursor: 'pointer' }}>Item List
+          {showItems ? <FaAngleUp /> : <FaAngleDown />}     
+        </h2>
+        {showItems && (
+          <ul>
+            {items.map((item, index) => (
+              <li key={index}>
+                <p>Name: {item.name}</p>
+                <p>Percentage Increase: {item.percentageIncrease}%</p>
+                <p>Total Price: ${calculateNewPrice(item.price, item.percentageIncrease).toFixed(2)}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  };
+function Part2({ isTableShown, setIsTableShown }) {
     return (
         <section className="part" id="part2">
             <div className="graph"></div>
             <div className="button-line">
-                <button onClick={() => setIsTableShown(true)}>표</button>
-                <button onClick={() => setIsTableShown(false)}>뉴스</button>
+            <button onClick={() => setIsTableShown(true)} style={{ borderBottom: isTableShown ? '5px solid #FFD601' : 'none' }}>표</button>
+                <button onClick={() => setIsTableShown(false)}style={{ borderBottom: isTableShown ? 'none' : '5px solid #FFD601' }}>뉴스</button>
             </div>
-            <div className="view" id={isTableShown ? 'table' : ''}></div>
-            <div className="view" id={isTableShown ? '' : 'news'}></div>
+            <div className="view" id={isTableShown ? 'table' : ''}>
+                <table>
+                    <tr>
+                        <th>종목명</th>
+                        <th>수량</th>
+                        <th>한 주당 가격</th>
+                        <th>매입 단가</th>
+                        <th>수익률</th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </table>
+            </div>
+            <div className="view" id={isTableShown ? '' : 'news'}>
+                <News title="제목" time="2024-09-08" sub="내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내" />
+                <News title="제목" time="2024-09-08" sub="내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내" />
+                <News title="제목" time="2024-09-08" sub="내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내" />
+                <News title="제목" time="2024-09-08" sub="내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내" />
+                <News title="제목" time="2024-09-08" sub="내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내내용용내" />
+            </div>
         </section>
     );
+}
+
+function News(props) {
+    return (
+        <div className='news'>
+            <img src='' title='ee'></img>
+            <div className='text'>
+                <h4>{props.title}</h4>
+                <h6>{props.time}</h6>
+                <h5>{props.sub}</h5>
+            </div>
+        </div>
+    )
 }
 
 function Part3() {
     return (
         <section className="part" id="part3">
-            <div className="interact"></div>
-            <div className="margin"></div>
-            <div className="next"></div>
+            <div className='quarter'>
+                <p>현재 분기</p>
+                <div className='quarter-list'>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </div>
+            </div>
+            <div className='list-grid'>
+                <List_money title="시드머니" money="0000" />
+                <List_money title="추정자산" money="0000" />
+                <List_money title="주문 가능 금액" money="0000" />
+                <List_money title="평가손익" money="0000" />
+                <div className='rate'>
+                    <p>수익률</p>
+                    <div className='percent'>
+                        <h3>100%</h3>
+                        <h2>100%</h2>
+                    </div>
+                </div>
+            </div>
+            <button>다음 분기</button>
         </section>
     );
 }
 
-function Detail({ isNextVisible, toggleNextVisibility, setIsTableShown }) {
+function List_money(props) {
     return (
-        <>
-            {isNextVisible && (
-                <div className="detail">
-                    <div className="paper">
-                        <div className="title">결과화면</div>
-                        <div className="result"></div>
-                        <div className="next">
-                            <button id="next-btn" onClick={() =>{ toggleNextVisibility(false); setIsTableShown(true);}}>다음화면</button>
-                        </div>
-                    </div>
-                </div>
-            )}
-        </>
-    );
+        <div className='grid'>
+            <div className='name'>
+                <p className='title'>{props.title}</p>
+                <icon>d</icon>
+            </div>
+            <div className='money'>{props.money}</div>
+        </div>
+    )
 }
-
 
 function App() {
     const [isNextVisible, setNextVisibility] = useState(false);
@@ -72,7 +199,6 @@ function App() {
                 <Part1 toggleNext={toggleNextVisibility} />
                 <Part2 isTableShown={isTableShown} setIsTableShown={setIsTableShown} />
                 <Part3 />
-                <Detail isNextVisible={isNextVisible} toggleNextVisibility={toggleNextVisibility} setIsTableShown={setIsTableShown} />
             </main>
         </>
     );
