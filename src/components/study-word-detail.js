e1c6ce700a101dadd9d4c450bf733060c831ff52
leeda06 <slmos33a@gmail.com>
2024-05-22 10:19:09 +0900

    📝 [feat] : localstorage에 저장된 결과값대로 단어나옴

--- src/components/study-word-detail.js
+++ src/components/study-word-detail.js
@@ -1,29 +1,68 @@
-import React from 'react'
-import { BsChevronLeft } from "react-icons/bs";
-import { BsChevronRight } from "react-icons/bs";
+import React, { useState, useEffect } from 'react';
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 import '../css/style.css';
 import '../css/study-word-detail.css';
+import wordData from '../Data/word.json';
 
 function StudyWordDetail() {
-  return (
-    <section id='studyGameDetail'>
-        <div className='StudyGameDetail__inner'>
-            <p className='round'><span className='roundWord'>1</span> / 10</p>
-            <div className='card_inner'>
-                <p><BsChevronLeft size="40"/></p>
-                <div className='card'>
-                    <div className='card_word'>ㅇ</div>
-                    <div className='card_word_exp'>ㅇ</div>
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [knownWords, setKnownWords] = useState([]);
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        const title = localStorage.getItem('memorizedTitle');
+        if (title) {
+            const titleObj = wordData.find(t => t.title === title);
+            if (titleObj) {
+                setCards(titleObj.cards);
+            } else {
+                setCards([]);
+            }
+        }
+    }, []);
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : cards.length - 1));
+    };
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex < cards.length - 1 ? prevIndex + 1 : 0));
+    };
+
+    const handleKnow = () => {
+        setKnownWords([...knownWords, currentIndex]);
+        handleNext();
+    };
+
+    const handleNextLater = () => {
+        handleNext();
+    };
+
+    if (cards.length === 0) {
+        return <div>선택한 제목에 대한 카드를 찾을 수 없습니다.</div>;
+    }
+
+    const currentCard = cards[currentIndex];
+
+    return (
+        <section id='studyGameDetail'>
+            <div className='StudyGameDetail__inner'>
+                <p className='round'><span className='roundWord'>{currentIndex + 1}</span> / {cards.length}</p>
+                <div className='card_inner'>
+                    <p onClick={handlePrev}><BsChevronLeft size="40" /></p>
+                    <div className='card'>
+                        <div className='card_word'>{currentCard.title}</div>
+                        <div className='card_word_exp'>{currentCard.content}</div>
+                    </div>
+                    <p onClick={handleNext}><BsChevronRight size="40" /></p>
                 </div>
-                <p><BsChevronRight size="40"/></p>
+                <div className='card_inner'>
+                    <button className='knowButton' onClick={handleKnow}>이제 알아요</button>
+                    {/* <button className='nextButton' onClick={handleNextLater}>다음에 할게요</button> */}
+                </div>
             </div>
-            <div className='card_inner'>
-            <button className='knowButton'>이제 알아요</button>
-            <button className='nextButton'>다음에 할게요</button>
-            </div>
-        </div>
-     </section>
-  )
+        </section>
+    );
 }
 
 export default StudyWordDetail;
