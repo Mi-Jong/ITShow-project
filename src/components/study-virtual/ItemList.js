08d4844b5c677eabc38e34a89f184f1edc74aec0
leeda06 <slmos33a@gmail.com>
2024-05-23 14:12:02 +0900

    :sparkles: [feat] : 가상 시뮬레이션 값변경

--- src/components/study-virtual/ItemList.js
+++ src/components/study-virtual/ItemList.js
@@ -3,21 +3,29 @@ import { FaAngleDown, FaAngleUp, FaSortDown, FaSortUp } from 'react-icons/fa';
 
 function ItemList() {
     const [items, setItems] = useState([
-        { name: 'Item 1', percentageIncrease: 10000, price: 100 },
-        { name: 'Item 2', percentageIncrease: -1500, price: 2000 },
-        { name: 'Item 3', percentageIncrease: 2000000, price: 150 },
-        { name: 'Item 1', percentageIncrease: -10000, price: 100 },
-        { name: 'Item 2', percentageIncrease: -1500, price: 200 },
-        { name: 'Item 3', percentageIncrease: -2000000, price: 150 }
+        { name: 'Item 1', percentageIncrease: 100, price: 100 },
+        { name: 'Item 2', percentageIncrease: 2000, price: 2000 },
+        { name: 'Item 3', percentageIncrease: 150, price: 150 }
     ]);
     const [showItems, setShowItems] = useState(false);
 
-    const calculateNewPrice = (price, percentageIncrease) => {
-        return (String)(price + percentageIncrease).replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
+    const calculateNewPrice = (price) => {
+        const newPrice = price;
+        return newPrice.toLocaleString();
     };
+    
 
     const toggleItems = () => {
         setShowItems(!showItems);
+    };
+
+    const updatePrices = () => {
+        const updatedItems = items.map(item => {
+            const change = 300;
+            const newPrice = item.price + change;
+            return { ...item, percentageIncrease: change, price: newPrice };
+        });
+        setItems(updatedItems);
     };
 
     return (
@@ -39,11 +47,12 @@ function ItemList() {
                                     {item.percentageIncrease}
                                 </p>
                             </div>
-                            <p className="pay-coin">{calculateNewPrice(item.price, item.percentageIncrease)}원</p>
+                            <p className="pay-coin">{calculateNewPrice(item.price)}원</p>
                         </li>
                     ))}
                 </ul>
             )}
+            <button onClick={updatePrices}>분기 업데이트</button>
         </div>
     );
 }
