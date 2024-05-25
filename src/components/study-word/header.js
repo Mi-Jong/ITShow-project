5c0fb795b0eaf596391f6da3ad66754a8ef19fa8
06shelk <shlee4349@gmail.com>
2024-05-25 20:57:55 +0900

    💄[design] : header 라우팅 및 studyGame css 수정

--- src/components/study-word/header.js
+++ src/components/study-word/header.js
@@ -1,32 +1,48 @@
 import React from 'react'
 import { IoExitOutline } from "react-icons/io5";
 import { GoQuestion } from "react-icons/go";
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const location = useLocation();
+  
+  const getLinkPath = () => {
+    switch (location.pathname) {
+      case '/StudyWord':
+        return '/';
+      case '/StudyWordDetail':
+        return '/StudyWord';
+      case '/StudyGame':
+        return '/StudyWord';
+      default:
+        return '/';
+    }
+  };
+
   return (
     <header id="header" role="banner">
-    <div className="header__inner">
-      <div className="header__logo">
-        <h1>
-          <a href="/">GEMMI</a>
-        </h1>
+      <div className="header__inner">
+        <div className="header__logo">
+          <h1>
+            <a href="/">GEMMI</a>
+          </h1>
+        </div>
+        <nav 
+          className= 'header__nav'
+          role="navigation" 
+          aria-label="메인메뉴"
+        >
+          <ul>
+              <li>
+                <GoQuestion size= "1.3em"/>
+              </li>
+              <li>
+                <Link to={getLinkPath()}><IoExitOutline size= "1.3em" /></Link>
+              </li>
+          </ul>
+        </nav>
       </div>
-      <nav 
-        className= 'header__nav'
-        role="navigation" 
-        aria-label="메인메뉴"
-      >
-        <ul>
-            <li>
-              <a href=""><GoQuestion size= "1.3em"/></a>
-            </li>
-            <li>
-              <a href=""><IoExitOutline size= "1.3em" /></a>
-            </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
+    </header>
   )
 }
 
