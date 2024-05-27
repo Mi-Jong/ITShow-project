1dea1704ea19453bae595f1533748e167311d4a1
06shelk <shlee4349@gmail.com>
2024-05-27 10:37:45 +0900

    ⚡[feat] : StudyWordDetail 의미보기 추가

--- src/components/study-word-detail.js
+++ src/components/study-word-detail.js
@@ -9,6 +9,7 @@ function StudyWordDetail() {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [knownWords, setKnownWords] = useState([]);
     const [cards, setCards] = useState([]);
+    const [isMeaningVisible, setIsMeaningVisible] = useState(false);
 
     useEffect(() => {
         const title = localStorage.getItem('memorizedTitle');
@@ -24,24 +25,30 @@ function StudyWordDetail() {
 
     const handlePrev = () => {
         setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : cards.length - 1));
+        setIsMeaningVisible(false);
     };
 
     const handleNext = () => {
         setCurrentIndex((prevIndex) => (prevIndex < cards.length - 1 ? prevIndex + 1 : 0));
+        setIsMeaningVisible(false);
     };
 
     const handleKnow = () => {
-        setKnownWords([...knownWords, currentIndex]);
-        handleNext();
+        setKnownWords((prevKnownWords) => {
+            const newKnownWords = [...prevKnownWords, currentIndex];
+            return newKnownWords;
+        });
+        setCards((prevCards) => {
+            const remainingCards = prevCards.filter((_, index) => index !== currentIndex);
+            setCurrentIndex(currentIndex < remainingCards.length ? currentIndex : 0);
+            return remainingCards;
+        });
     };
 
     const handleNextLater = () => {
         handleNext();
+        setIsMeaningVisible(false);
     };
-
-    if (cards.length === 0) {
-        return <div>선택한 제목에 대한 카드를 찾을 수 없습니다.</div>;
-    }
 
     const currentCard = cards[currentIndex];
 
@@ -51,19 +58,27 @@ function StudyWordDetail() {
             <section id='studyGameDetail'>  
                 <div class='studyGameDetail'>
                     <div className='StudyGameDetail__inner'>
-                        <p className='round'><span className='roundWord'>{currentIndex + 1}</span> / {cards.length}</p>
-                        <div className='card_inner'>
-                            <p onClick={handlePrev}><BsChevronLeft size="40" /></p>
-                            <div className='card'>
-                                <div className='card_word'>{currentCard.title}</div>
-                                <div className='card_word_exp'>{currentCard.content}</div>
-                            </div>
-                            <p onClick={handleNext}><BsChevronRight size="40" /></p>
-                        </div>
-                        <div className='card_inner'>
-                            <button className='knowButton' onClick={handleKnow}>이제 알아요</button>
-                            <button className='nextButton' onClick={handleNextLater}>다음에 할게요</button>
-                        </div>
+                        {cards.length === 0 ? (
+                            <p className='study_completed'>학습을 다 하셨습니다.</p>
+                        ) : (
+                            <>
+                                <p className='round'><span className='roundWord'>{currentIndex + 1}</span> / {cards.length}</p>
+                                <div className='card_inner'>
+                                    <p onClick={handlePrev}><BsChevronLeft size="40" /></p>
+                                    <div className='card'>
+                                        <div className='card_word'>{currentCard.title}</div>
+                                        {isMeaningVisible ? <div className='card_word_exp'>{currentCard.content}</div> : <div className='card_word_exp'></div>}
+                                    </div>
+                                    <p onClick={handleNext}><BsChevronRight size="40" /></p>
+                                </div>
+                                <div className='card_inner'>
+                                    {!isMeaningVisible && <button className='knowButton' onClick={() => setIsMeaningVisible(true)}>의미보기</button>}
+                                    {isMeaningVisible && <button className='knowButton' onClick={handleKnow}>이제 알아요</button>}
+                                    {isMeaningVisible && <button className='nextButton' onClick={handleNextLater}>다음에 할게요</button>}
+                                </div>
+                            </>
+                        )}
+                       
                     </div>
                 </div>
             </section>
