c8cded507047e63651af761db96b3c4fd3d4a112
06shelk <shlee4349@gmail.com>
2024-05-29 10:54:02 +0900

    ⚡ [feat] : news image 추가

---
+++ src/Data/news.js
@@ -0,0 +1,45 @@
+import enter01 from "../img/news/news-enter01.png"
+import enter02 from "../img/news/news-enter02.png"
+import enter03 from "../img/news/news-enter03.png"
+import enter04 from "../img/news/news-enter04.png"
+import enter05 from "../img/news/news-enter05.png"
+import enter06 from "../img/news/news-enter06.png"
+import enter07 from "../img/news/news-enter07.png"
+
+export const newsData = [
+    {
+      image_url: enter01,
+      title: "SN 에스퍼 윈퉈, 첫 솔로 엘범 발표하자마자 음원차트 ‘1위’",
+      content: "[엔터기자] 이수만 기자 = SN 소속 에스퍼 윈퉈가 솔로 가수 ‘김앤정’으로서 성공적인 출발을 알렸다. 지난 xx일 윈퉈는 자신의 인스타그램에 앨범을 암시하는 내용이 담긴 게시물을 올렸다. 공개된 사진 속에는 ‘겨울 속에서’라는 가사지가 눈길을 이끌었다. "
+    },
+    {
+      image_url: enter02,
+      title: "‘샤이닝 자물쇠’ 열애설 인정에 팬들 광분",
+      content: "[유앤아이 AI리포터] 최근 샤이닝 멤버 자물쇠가 배우 XXX과의 열애 사실을 인정하면서 팬들로부터의 질타를 받고 있다. ‘배우' XXX는 온라인에서 자신을 비방하고 허위 사실을 포함한 악의적 의도의 모욕적인 게시글에 대한 법적 대응을 발표했다."
+    },
+    {
+      image_url: enter06,
+      title: "지닌, 끝내 밝히지 못한 ‘앤드믹스 탈퇴’ 이유",
+      content: "[앳스타일 홍길동 기자] jyb엔터테이먼트에 10년을 몸담았던 지닌은 한순간에 떠났다. 기다렸던 그룹 앤드믹스로 데뷔했건만 6개월도 채우기 못한 채 탈퇴했다. 반면 그룹 앤드믹스 한원이 지난해 말 탈퇴한 지닌에 대한 질문에 입을 열었다."
+    },
+    {
+      image_url: enter04,
+      title: "‘jyb 신인’ 아일럿, 데뷔 타이틀 곡은 ‘KOSPI’",
+      content: "‘하이브 막내딸’이라고 불리는 신인 걸그룹 아일럿(eyelet)의 데뷔 타이틀 곡은 ‘KOSPI’이다. 아일럿(헤이즐넛, 라떼, 모카)은 x일 x시 공식 SNS에 데뷔 엘범인 미니 1집 ‘Stock’의 세번째 콘셉트 ‘개미’ 버전 포토와 필름을 공개했다."
+    },
+    {
+      image_url: enter05,
+      title: "MG, SL ‘마약 입건’ 승승장구하던  MG엔터 하락",
+      content: "가수 SL의 마약 투약 혐의 파장으로 소속사 MG엔터테인먼트 주가가 급락 사태를 맞았다. 이는 전날 SL의 마약 투약 혐의 입건 사실이 알려진 여파다. 서울경찰청 광역수사대는 최근 SL를 마약류관리에관한법률위반 혐의로 불구속 입건했고 해당 사실이 XX일 기사화됐다."
+    },
+    {
+      image_url: enter03,
+      title: "봥탄 군입대 소식에 기업 주가 하락",
+      content: "군입대를 앞둔 봥탄의 소속사 기업이 해당 소식에 따라 주가가 하락세를 보이고 있다. 팬들과 시장 참여자들은 그룹의 활동 중단으로 인한 영향에 대해 우려하며, 기업은 이에 대응하여 봥탄 의 군입대 기간 동안도 효과적인 전략을 모색하고 있다"
+    },
+    {
+      image_url: enter07,
+      title: "남자친구, 태도 논란에 어떻게 대응할까",
+      content: "SN 소속 인기 아이돌 그룹 남자친구가 최근 공개된 영상에서 태도 논란에 휩싸였다. 팬들은 멤버들의 행동에 대해 논란을 제기하고 있으며, 소속사는 이에 대한 공식 입장 발표를 약속하고 있다. 그룹은 논란에 대한 해명과 팬들에 대한 사죄를 통해 평가를 받아야 할 상황에 처해 있어, 이에 대한 대응이 기대된다."
+    }
+  ]
