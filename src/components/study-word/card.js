a38adfc03d28018bfac465e07262d9d16397ee1f
06shelk <shlee4349@gmail.com>
2024-05-30 23:53:07 +0900

    ✨ [feat] : 단어 클릭 설명 화면

--- src/components/study-word/card.js
+++ src/components/study-word/card.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GoX } from "react-icons/go";
 
 const Card = ({ title, content, query }) => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(true);
+    };
+
+    const handleClickX = () => {
+        setIsClicked(false);
+    };
+
     return (
-        <div className="card-n">
-            <p id='h3'>{highlightMatch(title, query)}</p>
-        </div>
+        <>
+            <div onClick={handleClick}  className="card-n">
+                <p id='h3'>{highlightMatch(title, query)}</p>
+            </div>
+            {isClicked &&   <ClickCard title={title} content={content} onClickX={handleClickX}/>}
+        </>
+      
     );
 };
 
@@ -16,5 +31,23 @@ const highlightMatch = (text, query) => {
         part.toLowerCase() === query.toLowerCase() ? <span key={index} style={{ backgroundColor: 'yellow' }}>{part}</span> : part
     );
 };
+
+function ClickCard({ title, content, onClickX  }) {
+    return (
+        <section id="clickCard" className='clickCard'>
+            <div className='clickCard__inner'>
+                <div className='x-cont' >
+                    <p>
+                        <GoX size="40" onClick={onClickX}/>
+                    </p>
+                </div>
+                <div className='innerText'>
+                    <h1>{title}</h1>  
+                    <p>{content}</p>  
+                </div>
+            </div>
+        </section>
+    );
+}
 
 export default Card;
